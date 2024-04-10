b34bb9d49d8c390a42831e28b46b91cdba9fdf1d
Sanju <sanjuafre08@gmail.com>
2024-04-10 09:55:39 +0530

    10/04/2024

---
+++ lib/home/componets/Row5_Row6.dart
@@ -0,0 +1,210 @@
+
+import 'package:flutter/material.dart';
+
+Row Row5() {
+  return Row(
+    children: [
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(Icons.add, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.radio_button_checked, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.keyboard_arrow_left, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.keyboard_arrow_right, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(Icons.alarm, color: Color(0xFF616161), size: 37,),
+      ),
+    ],
+  );
+}
+
+Row Row6() {
+  return Row(
+    children: [
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(Icons.add, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.radio_button_checked, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.keyboard_arrow_left, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.keyboard_arrow_right, color: Color(0xFF616161), size: 37,),
+      ),
+
+      Container(
+        margin: const EdgeInsets.all(10),
+        height: 105,
+        width: 105,
+        decoration: BoxDecoration(
+          color: const Color(0xFFf5f5f5),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.teal,
+              offset: Offset.fromDirection(BorderSide.strokeAlignOutside),
+              blurRadius: 8,
+
+            ),
+          ],
+        ),
+        child: const Icon(Icons.alarm, color: Color(0xFF616161), size: 37,),
+      ),
+    ],
+  );
+}
